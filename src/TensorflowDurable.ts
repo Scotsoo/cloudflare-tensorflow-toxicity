dbe2281535586fa15b6f708b187b9b632c03ba20
Jack Scotson <jackscotson@gmail.com>
2021-08-24 15:10:13 +0100

    init code

---
+++ src/TensorflowDurable.ts
@@ -0,0 +1,67 @@
+import type tensorflow from '@tensorflow/tfjs'
+import * as toxicity from '@tensorflow-models/toxicity'
+interface Value {
+  toxicityModel: toxicity.ToxicityClassifier
+}
+
+interface RequestBody {
+  messages: string[]
+}
+
+const THRESHOLD = 0.7
+export class TensorflowDurable {
+  initializePromise?: Promise<void>
+  value!: Value
+  state: DurableObjectState
+  env: { [key: string]: string }
+  constructor(state: DurableObjectState, env: any) {
+    this.state = state;
+    this.env = env
+  }
+
+  async loadModel (): Promise<toxicity.ToxicityClassifier> {
+    console.log('loading model')
+    const model = await toxicity.load(THRESHOLD, [])
+    console.log('model loaded')
+    return model
+  }
+
+  async initialize() {
+    console.log('init called')
+    let stored = await this.state.storage.get("state") as Value | undefined
+    const model = await fetch(new Request('https://tfhub.dev/tensorflow/tfjs-model/tutorials/spam-detection/tfjs/1'))
+    console.log('model', model)
+    // @ts-ignore
+    this.value = stored
+    // || {
+    //   toxicityModel: await this.loadModel()
+    // }
+    console.log('init fin')
+
+  }
+
+  async fetch(request: Request) {
+    if (this.initializePromise === undefined) {
+      this.initializePromise = this.initialize()
+      .catch((err) => {
+        console.log('CAUGHT THE ERROR', err)
+        // If anything throws during initialization then we need to be
+        // sure sure that a future request will retry initialize().
+        // Note that the concurrency involved in resetting this shared
+        // promise on an error can be tricky to get right -- we don't
+        // recommend customizing it.
+        this.initializePromise = undefined;
+        throw err
+      })
+    }
+    console.log('awaiting init')
+    await this.initializePromise;
+    console.log('after init')
+    const data = await request.json() as RequestBody
+    return new Response(JSON.stringify(data))
+    return new Response(JSON.stringify(await this.value.toxicityModel.classify(data.messages)))
+
+  }
+
+
+}
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+// @ts-ignore
+import * as process from 'process'
+// In order for the workers runtime to find the class that implements
+// our Durable Object namespace, we must export it from the root module.
+export { TensorflowDurable } from './TensorflowDurable'
+const oldReq = Request
+// @ts-ignore
+Request.prototype = (...args) => {
+  console.log('fetch called', args)
+  // @ts-ignore
+  return oldReq.prototype.apply(...args)
+}
+export default {
+  async fetch(request: Request, env: Env) {
+    try {
+      return await handleRequest(request, env)
+    } catch (e) {
+      return new Response(e)
+    }
+  },
+}
+
+async function handleRequest(request: Request, env: Env) {
+  let id = env.TENSORFLOWDURABLE.idFromName('TENSORFLOW')
+  let obj = env.TENSORFLOWDURABLE.get(id)
+  let resp = await obj.fetch(request)
+  return resp
+}
+
+interface Env {
+  TENSORFLOWDURABLE: DurableObjectNamespace
+}
