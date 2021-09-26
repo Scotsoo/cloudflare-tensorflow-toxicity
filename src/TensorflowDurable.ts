bbcb704b82d6e4463eb3c90e435155eab4bdbba5
Jack Scotson <jackscotson@gmail.com>
2021-09-26 17:56:03 +0100

    currently won't work inc loudflare due to size constraints

--- src/TensorflowDurable.ts
+++ src/TensorflowDurable.ts
@@ -1,19 +1,5 @@
 import * as tensorflow from '@tensorflow/tfjs'
 import * as toxicity from '@tensorflow-models/toxicity'
-tensorflow.setPlatform('cloudflare', {
-  fetch: fetch,
-  now: Date.now,
-  // @ts-ignore
-  decode: (text: string, encoding: string) => {
-    // @ts-ignore
-    return new Buffer(text, encoding).toString()
-  },
-  // @ts-ignore
-  encode: (bytes: Uint8Array, encoding: string) => {
-    // @ts-ignore
-    return new Buffer(bytes).toString(encoding)
-  }
-})
 interface Value {
   toxicityModel: toxicity.ToxicityClassifier
 }
@@ -28,29 +14,77 @@ export class TensorflowDurable {
   value!: Value
   state: DurableObjectState
   env: { [key: string]: string }
+  toxicity!: toxicity.ToxicityClassifier
+  async toxicityFetch (self: this, path: string, requestInits?: RequestInit | undefined, options?: tensorflow.io.RequestDetails | undefined) {
+    const stored = await this.state.storage.get(path) as ArrayBuffer
+    if (stored !== undefined) {
+      return new Response(stored)
+    }
+    console.log('path', path)
+    const data = await fetch(path, requestInits)
+    const str = await data.arrayBuffer()
+    console.log(path, 'bytes', str.byteLength)
+    await this.state.storage.put(path, str)
+    return new Response(str)
+    // return await fetch(path, requestInits)
+  }
+
+
   constructor(state: DurableObjectState, env: any) {
     this.state = state;
     this.env = env
+    tensorflow.setPlatform('cloudflare', {
+      // @ts-ignore
+      fetch: (path, requestInits, c) => {
+
+        return this.toxicityFetch(this, path, requestInits)
+      },
+      now: Date.now,
+      // @ts-ignore
+      decode: (text: string, encoding: string) => {
+        // @ts-ignore
+        return new Buffer(text, encoding).toString()
+      },
+      // @ts-ignore
+      encode: (bytes: Uint8Array, encoding: string) => {
+        // @ts-ignore
+        return new Buffer(bytes).toString(encoding)
+      },
+      loadInSerial: true
+    })
+    tensorflow.setBackend('cpu')
   }
 
   async loadModel (): Promise<toxicity.ToxicityClassifier> {
-    console.log('loading model')
     const model = await toxicity.load(THRESHOLD, [])
-    console.log('model loaded')
     return model
   }
 
   async initialize() {
-    console.log('init called')
-    let stored = await this.state.storage.get("state") as Value | undefined
-    this.value = stored || {
-      toxicityModel: await this.loadModel()
-    }
-    console.log('init fin')
-
+    // let stored = await this.state.storage.get("state") as Value | undefined
+    // this.value = stored || {
+    //   toxicityModel: await this.loadModel()
+    // }
+    const loadStart = Date.now()
+    console.log('Loading model', loadStart)
+    this.toxicity = await this.loadModel()
+    console.log('loaded model', Date.now() - loadStart)
+    // this.toxicityModel = this.loadModel()
+    // console.log('MODEL', this.value.toxicityModel)
+    //   if (stored === undefined) {
+    //     // @ts-ignore
+    //     delete this.value.toxicityModel.model.handler.fetch
+    //     this.state.storage.put('state', this.value)
+    //   }
+    //   // @ts-ignore
+    //   this.value.toxicityModel.model.handler.fetch = toxicityFetch
   }
 
   async fetch(request: Request) {
+    if (request.headers.get('purge') === 'PURGE') {
+      await this.state.storage.delete('state')
+      return new Response('We purged baby!!')
+    }
     if (this.initializePromise === undefined) {
       this.initializePromise = this.initialize()
       .catch((err) => {
@@ -64,13 +98,21 @@ export class TensorflowDurable {
         throw err
       })
     }
-    console.log('awaiting init')
+    const modelLoadStart = Date.now()
     await this.initializePromise;
-    console.log('after init')
+    const modelLoadEnd = Date.now()
     const data = await request.json() as RequestBody
-    // return new Response(JSON.stringify(data))
-    return new Response(JSON.stringify(await this.value.toxicityModel.classify(data.messages)))
-
+    const now = Date.now()
+    console.log(`starting clasification`, now)
+    // @ts-ignore
+    const clasification = await this.toxicity.classify(data.messages)
+    // @ts-ignore
+    console.log(`clasification took`, Date.now() - now)
+    const headers = new Headers()
+    headers.set('content-type', 'application/json')
+    return new Response(JSON.stringify({ clasification, CLASSIFICATION_TIME_TAKEN: Date.now() - now, MODEL_LOAD_TIME: modelLoadEnd - modelLoadStart }), {
+      headers
+    })
   }
 
 
